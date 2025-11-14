c7199d368e4fcca86c78f80afbfb3815bb3adb14
samboadas221 <samboadas2221@gmail.com>
2025-11-14 06:59:38 -0400

    Basic game screen. Compilation try #1

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'CrossMathMisWebos',
+      title: 'CrossMath',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.indigo),
       home: HomeScreen(),
--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -1,19 +1,23 @@
 import 'package:flutter/material.dart';
+import 'game_screen.dart';
+import 'stats_screen.dart';
+import '../../domain/puzzle_model.dart'; // para Difficulty
 
 class HomeScreen extends StatelessWidget {
-  Widget _bigButton(BuildContext c, String label, Color color) {
-    return Container(
-      width: double.infinity,
-      margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 24.0),
-      child: RaisedButton(
-        onPressed: () {}, // placeholder
-        color: color,
-        textColor: Colors.white,
-        padding: EdgeInsets.symmetric(vertical: 18.0),
-        child: Text(label, style: TextStyle(fontSize: 18.0)),
-      ),
-    );
-  }
+  
+  Widget _bigButton(BuildContext c, String label, Color color, VoidCallback onPressed) {
+  return Container(
+    width: double.infinity,
+    margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 24.0),
+    child: RaisedButton(
+      onPressed: onPressed,
+      color: color,
+      textColor: Colors.white,
+      padding: EdgeInsets.symmetric(vertical: 18.0),
+      child: Text(label, style: TextStyle(fontSize: 18.0)),
+    ),
+  );
+}
 
   @override
   Widget build(BuildContext context) {
@@ -22,9 +26,19 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           children: <Widget>[
             SizedBox(height: 12.0),
-            _bigButton(context, 'FÁCIL', Colors.green),
-            _bigButton(context, 'MEDIO', Colors.orange),
-            _bigButton(context, 'DIFÍCIL', Colors.red),
+            
+            _bigButton(context, 'Fácil', Colors.green, () {
+              Navigator.of(context).push(MaterialPageRoute(builder: (_) => GameScreen(difficulty: Difficulty.easy)));
+            }),
+            
+            _bigButton(context, 'Medio', Colors.orange, () {
+              Navigator.of(context).push(MaterialPageRoute(builder: (_) => GameScreen(difficulty: Difficulty.medium)));
+            }),
+            
+            _bigButton(context, 'Difícil', Colors.red, () {
+              Navigator.of(context).push(MaterialPageRoute(builder: (_) => GameScreen(difficulty: Difficulty.hard)));
+            }),
+            
             Expanded(child: Container()), // pushes stats button to bottom
             Container(
               alignment: Alignment.centerRight,
